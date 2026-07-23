e5892a699d18df693fa511d697f58021094b1cff
Fercr2000 <fernandojosecamachorizquez@gmail.com>
2026-07-23 13:58:58 +0300

    actualización

--- app/(sitio)/page.tsx
+++ app/(sitio)/page.tsx
@@ -2,6 +2,7 @@ import Hero from "../components/hero";
 import About from "../components/about";
 import Director from "../components/palabrasDirector";
 import Inscription from "../components/inscripcion";
+import Integrantes from "../components/integrantes";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Director />
       <Inscription />
+      <Integrantes />
     </main>
   );
 }
